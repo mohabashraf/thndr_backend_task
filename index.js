2b59e51f61f8a63be41c57365814b0d8080ec4ab
mohabashraf <mohabashraf466@gmail.com>
2021-07-14 23:43:37 +0200

    change mqtt subscribe

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/values/all", async (req, res) => {
 
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
-    res.send(values);
+    res.json(values);
   });
 });
 
