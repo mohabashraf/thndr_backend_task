3cf59353ccf7599e6bde56a9175da21e5369891e
mohabashraf <mohabashraf466@gmail.com>
2021-07-15 18:42:19 +0200

    handle error empty stock

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get("/", (req, res) => {
 
 app.get("/values/all", async (req, res) => {
   const values = await pgClient
-    .query("SELECT * from stock_analysis ")
+    .query("SELECT * from stocks")
     .catch((err) => console.log(err));
   res.send(values.rows);
 });
